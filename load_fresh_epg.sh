9c6dd4f557bb5df7c2e5fe813b03e0f57a99ebad
Jason vanValkenburgh <23403940+jvanvalkenburgh@users.noreply.github.com>
2021-04-20 13:52:00 -0400

    Update load_fresh_epg.sh
    updated for container

--- load_fresh_epg.sh
+++ load_fresh_epg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /home/jvanvalkenburgh/git/plutoTV-tvheadend
+cd /plutoTV-tvheadend
 # when programn guide is regenb'ed, the url params change.  this makes tvheadend
 # think its a newux, and purges all of the "old" muxes
 # perl plutotv-generate.pl --createm3u --usestreamlink   #  --useffmpeg
 perl plutotv-generate.pl --usestreamlink   #  --useffmpeg
-cat plutotv-epg.xml | socat - UNIX-CONNECT:/home/jvanvalkenburgh/.hts/tvheadend/epggrab/xmltv.sock
-cat plutotv-epg.xml | socat - UNIX-CONNECT:/home/jvanvalkenburgh/.hts/tvheadend/epggrab/xmltv.sock
+cat plutotv-epg.xml | socat - UNIX-CONNECT:/config/.hts/tvheadend/epggrab/xmltv.sock
+cat plutotv-epg.xml | socat - UNIX-CONNECT:/config/.hts/tvheadend/epggrab/xmltv.sock
