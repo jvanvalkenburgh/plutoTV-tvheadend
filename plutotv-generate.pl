a9112767b147d98d09801e22eb7a2ccb0565705c
Jason vanValkenburgh <jasonv@cloudistics.com>
2021-04-25 23:16:36 -0400

    fix

--- plutotv-generate.pl
+++ plutotv-generate.pl
@@ -157,11 +157,11 @@ if ($response->is_success) {
                         #  using settings from  https://stackoverflow.com/questions/54700933/ffmpeg-dts-delta-threshold-and-aresample-async-1
                         #  based on issues wuith sync
 			            ##print $fhm "pipe://".$streamlink." --stdout --quiet --twitch-disable-hosting --ringbuffer-size 8M --hds-segment-threads 2 --hls-segment-attempts 2 --hls-segment-key-uri \"\" --hls-segment-timeout 5 \"".$url."\" 720,best\n";
-                        
+
                         print $fhm "pipe://".$streamlink." --stdout --quiet --twitch-disable-hosting ".
                             "--ringbuffer-size 64M --hds-segment-threads 2 --hls-segment-attempts 5 " .
                             "--hls-segment-timeout 5 --hls-timeout 100000000 --hls-live-restart \"".$url."\" best" .
-                            "| ffmpeg -loglevel fatal -err_detect ignore_err " .
+                            "| ffmpeg --loglevel fatal -err_detect ignore_err " .
                             "-f mpegts -i - " .
                             "-c:v copy -tune zerolatency -pix_fmt yuv420p -force_key_frames \"expr:gte(t,n_forced*2)\" ".
                             "-c:a aac -copyts -qscale:s 2 -b:a 256k -ac 2 -af aresample=async=1 -adrift_threshold 0.1 " .
